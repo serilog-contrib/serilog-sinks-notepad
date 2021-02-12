1549ca656f11d055f47037af6ca3c48dd28d8880
C. Augusto Proiete <augusto@proiete.com>
2021-02-12 01:43:50 -0400

    Update copyright year

--- src/Serilog.Sinks.Notepad/Sinks/TextWriter/TextWriterSink.cs
+++ src/Serilog.Sinks.Notepad/Sinks/TextWriter/TextWriterSink.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2020 C. Augusto Proiete & Contributors
+﻿#region Copyright 2020-2021 C. Augusto Proiete & Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+#endregion
 
 using System;
 using Serilog.Core;
